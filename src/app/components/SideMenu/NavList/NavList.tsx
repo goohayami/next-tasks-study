7cd0035162aa2b482f2294f85410bf5991064e9d
hayami <goohayami@gmail.com>
2024-09-14 23:08:57 +0900

    first comitte

---
+++ src/app/components/SideMenu/NavList/NavList.tsx
@@ -0,0 +1,36 @@
+import { FaTasks, FaRegCheckSquare, FaRegClock } from "react-icons/fa";
+import NavItem from "./NavItem/NavItem";
+
+interface NavItemType {
+  id: number;
+  label: string;
+  link: string;
+  icon: React.ReactNode;
+}
+
+const NavList = () => {
+  const navlList: NavItemType[] = [
+    { id: 1, label: "All tasks", link: "/", icon: <FaTasks /> },
+    {
+      id: 2,
+      label: "Completed Tasks",
+      link: "/comleted",
+      icon: <FaRegCheckSquare />,
+    },
+    { id: 3, label: "Expired tasks", link: "/expired", icon: <FaRegClock /> },
+  ];
+  return (
+    <div className="mt-24">
+      {navlList.map((item) => (
+        <NavItem
+          key={item.id}
+          label={item.label}
+          link={item.link}
+          icon={item.icon}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default NavList;
