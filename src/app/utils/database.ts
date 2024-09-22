01e9531f452fd2541191567f28e5b80fbf540e20
hayami <goohayami@gmail.com>
2024-09-23 00:09:07 +0900

    .env

--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
-  console.log(process.env.DB_URI);
-  const db_url =
-    "mongodb+srv://hayami:metamask2jp@cluster0.vcjel.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   try {
-    await mongoose.connect(db_url || "");
+    await mongoose.connect(process.env.DB_URI || "");
   } catch (error) {
     console.log("DB接続に失敗しました");
     throw new Error();
