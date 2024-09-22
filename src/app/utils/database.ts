914c2ca9f2494b7880b39ea4bdd0ccfe7920c0ee
hayami <goohayami@gmail.com>
2024-09-23 00:18:41 +0900

    agda

--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
+  console.log(process.env.DB_URI);
   try {
     await mongoose.connect(process.env.DB_URI || "");
   } catch (error) {
